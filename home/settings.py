270e4adb0a9e790f9dd187c719728e84f1a33258
João Sabino <jsabino2@gmail.com>
2022-01-10 21:54:03 +0000

    Fix warnings for auto-created primary keys.

--- home/settings.py
+++ home/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'home.wsgi.application'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
