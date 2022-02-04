f3a6d07841130efc6f1e752002083c363e15ca19
João Sabino <jsabino2@gmail.com>
2022-02-04 21:27:22 +0000

    Add solar production data.

--- manage.py
+++ manage.py
@@ -2,12 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import logging
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.settings')
-    # logging.basicConfig(level=logging.INFO)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- scripts/clean_setup.ps1
+++ scripts/clean_setup.ps1
@@ -1,7 +1,10 @@
+# Run from root project folder
+
 Remove-Item .\scheduler\migrations\0*
 Remove-Item *.sqlite3
 python manage.py makemigrations
 python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
 python manage.py loaddata default_appliances.json
+echo 'exec(open("scripts/load_solar_data.py").read())' | python manage.py shell
 # python manage.py runserver
